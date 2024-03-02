3e8dcff698a1eb82e2f9a376e9057e8d902a6189
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2024-03-02 15:20:16 +0100

    feat: authtification
    Add login and signup logic and page (/login and /signup)

---
+++ src/routes/(auth)/login/+page.server.ts
@@ -0,0 +1,48 @@
+import { redirect } from '@sveltejs/kit';
+
+export const actions = {
+    default: async ({ request, locals }) => {
+        if (locals.pocketBase.authStore.isValid) {
+            return;
+        }
+
+        const formData = await request.formData();
+
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        try {
+            if (typeof email !== 'string') {
+                throw new Error('Email must be a string');
+            }
+            if (email.length < 5) {
+                throw new Error('Please enter a valid email address');
+            }
+
+            if (typeof password !== 'string') {
+                throw new Error('Password must be a string');
+            }
+            if (password.length < 8) {
+                throw new Error('Password must be at least 8 characters long');
+            }
+
+            await locals.pocketBase.collection("users").authWithPassword(email, password);
+        } catch (error) {
+            if (!(error instanceof Error)) {
+                return {
+                    email,
+                    password,
+                    error: "Unknown error occured when signing up user"
+                }
+            }
+
+            return {
+                error: error.message,
+                email,
+                password,
+            };
+        }
+
+        redirect(303, "/")
+    }
+}
