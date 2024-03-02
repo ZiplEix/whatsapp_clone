06ee41588e47dba894a7deb9bf4a1217752409dc
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2024-03-02 17:09:56 +0100

    feat: root protection for main page

---
+++ src/routes/(app)/+layout.server.ts
@@ -0,0 +1,19 @@
+import { redirect } from "@sveltejs/kit";
+import type { LayoutServerLoad } from "../(auth)/$types";
+import { validateUser } from "$lib/types";
+
+export const load = (async ({ locals }) => {
+    const authModel = locals.pocketBase.authStore.model
+
+    if (!authModel) {
+        redirect(303, "/signup");
+    }
+
+    if (!validateUser(authModel)) {
+        redirect(303, "/signup");
+    }
+
+    return {
+        authModel,
+    }
+}) satisfies LayoutServerLoad
